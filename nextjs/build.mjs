da9c3f14f39581a172086fdfb04d1784a535a475
larry <xspanni@gmail.com>
2024-09-09 17:04:28 +0800

    清理无关文件和代码

--- nextjs/build.mjs
+++ nextjs/build.mjs
@@ -1,19 +1,17 @@
-#!/usr/bin/env -S deno run --allow-env --allow-run --allow-read --allow-write
-
-import {$, cd} from 'https://deno.land/x/zx_deno/mod.mjs'
+import {$, cd} from 'zx'
 
 await $`date`
 
 async function buildApp() {
     await $`npm run build`
-    await $`docker build -t calieo-nextjs -f Dockerfile .`
+    await $`docker build -t calieo-password -f Dockerfile .`
 
     // 集成环境下重启容器
-    await $`docker rm -f calieo-nextjs`
+    await $`docker rm -f calieo-password`
     await $`docker run -d --restart=always \
-            --name calieo-nextjs \
-            -p 8000:8000 \
-            calieo-nextjs`
+            --name calieo-password \
+            -p 8003:8003 \
+            calieo-password`
 }
 
 await buildApp()
